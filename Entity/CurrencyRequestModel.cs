149943e1d24c0b4583e72f3926e1721646625eeb
UOzgurYildiz <uozguryildiz@hotmail.com>
2024-01-08 22:26:27 +0300

    commit3

--- Entity/CurrencyRequestModel.cs
+++ Entity/CurrencyRequestModel.cs
@@ -4,5 +4,5 @@ public class CurrencyRequestModel
 {
     public string FromCurrency { get; set; }
     public string ToCurrency { get; set; }
-    public int MoneyAmount { get; set; }
+    public decimal Amount { get; set; }
 }
