3832950163802135c3be559258c6132ad086fa36
Uğur Özgür Yıldız <96427179+UOzgurYildiz@users.noreply.github.com>
2024-01-10 15:22:44 +0300

    Update Config.cs

--- ApplicationConfig/Config.cs
+++ ApplicationConfig/Config.cs
@@ -3,5 +3,5 @@ namespace CurrencyApplication.ApplicationConfig;
 public class Config
 {
     public static readonly string BaseURL = "https://currency-conversion-and-exchange-rates.p.rapidapi.com";
-    public  static readonly string APIKEY="43ec4d652dmshf128ad18dc0d7c3p144ab6jsn219d44a5a794";
+    public  static readonly string APIKEY="-";
 }
